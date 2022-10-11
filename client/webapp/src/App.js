85054fc59fcb17576fdba6a2157819f74e3a2299
Zachary Gressaffa <zachary.gressaffa@selu.edu>
2022-10-11 00:36:48 -0500

    fix pt 3

--- client/webapp/src/App.js
+++ client/webapp/src/App.js
@@ -1,13 +1,11 @@
 import './App.css'
 import LoggedIn from './LoggedIn'
-import React, {useEffect, useState} from "react";
+import React from "react";
 import { useAuth } from "./hooks";
 import {Link, Route, Routes, useNavigate} from "react-router-dom";
 import './Landing.css'
-import Nav from "react-bootstrap/Nav";
-import Login from "./NotLoggedIn/Login";
-import Signup from "./NotLoggedIn/Signup";
-import Landing from "./Landing";
+import SignupPage from "./NotLoggedIn/SignupPage";
+import SigninPage from './NotLoggedIn/SigninPage';
 
 function App() {
 
@@ -16,31 +14,21 @@ function App() {
     const {isPending, isLoggedIn} = authInfo;
     const navigate = useNavigate();
 
-    const isAdmin = authInfo.profile?.role === ("admin" || "user");
+    const isAdmin = authInfo.profile?.role === ("admin");
 
     if (isLoggedIn) return <LoggedIn/>
 
     return (
         <>
-            <Landing/>
-                <div className='inner'>
-                    <Routes>
-                        <Route path="/signin" element={<Login />} />
-                    </Routes>
-                    <Routes>
-                        <Route path="/signup" element={<Signup />} />
-                    </Routes>
-                </div>
-
+            <div className='app'>
+                <Routes>
+                    <Route path="/" element={<SigninPage />} />
+                </Routes>
+                <Routes>
+                    <Route path="/signup" element={<SignupPage />} />
+                </Routes>
+            </div>
         </>
     );
 }
 export default App;
-
-/*
-{ (isAdmin) ? (
-            <LoggedIn/>
-        ) : (
-            <NotLogged/>
-        )}
- */
