90ea96152258f359867036fc478b7a6eebc2aae5
Alex Santana <alexsantanadev@gmail.com>
2022-10-27 01:35:03 -0500

    adding menu items

---
+++ client/webapp/src/testBeverages.js
@@ -0,0 +1,28 @@
+const drinksArray = [
+    {
+        id: 1,
+        title: 'Ham Drink',
+        price: 4.99,
+        img: 'https://images.unsplash.com/photo-1567234669003-dce7a7a88821?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+    },
+    {
+        id: 2,
+        title: 'Druink',
+        price: 3.99,
+        img: 'https://images.unsplash.com/photo-1550304943-4f24f54ddde9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+    },
+    {
+        id: 3,
+        title: 'Dr Wings',
+        price: 7.99,
+        img: 'https://images.unsplash.com/photo-1600555379765-f82335a7b1b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+    },
+    {
+        id: 4,
+        title: 'awda Nuggies',
+        price: 3.99,
+        img: 'https://images.unsplash.com/photo-1562967914-01efa7e87832?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1473&q=80'
+    },
+]
+
+export default drinksArray;
